a84a76201d05182f7f6c540d5116579584298005
Qining Lu <lqn.anthony@gmail.com>
2020-08-03 21:29:50 +0000

    add dockerfile for testing setup_ubuntu

---
+++ setup/test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+exe() { echo "\$ $@"; "$@"; }
+
+trap 'echo Ctrl-c, Test interrupted; exit' INT
+
+echo -e \
+  "
+  ##############
+  ## Checking ##
+  ##############
+  "
+exe docker run hello-world && \
+exe tmux -V && \
+exe nordvpn --version && \
+
+if [ $? -eq 0 ]; then
+  echo -e \
+  "
+  #####################
+  ## All Checks Done ##
+  #####################
+  "
+  exit 0
+fi
