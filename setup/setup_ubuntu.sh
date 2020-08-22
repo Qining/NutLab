476e1e2c1b535aeb88b5c312373b2fb5f17a2974
Qining <lqn.anthony@gmail.com>
2020-08-22 19:34:12 +0000

    minor

--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -10,7 +10,7 @@ echo "${me} -- Start"
 
 # basic
 sudo apt-get update
-sudo apt-get install -y curl git wget gnupg2 net-tools certbot python3-certbot-nginx sysstat
+sudo apt-get install -y curl git wget gnupg2 net-tools certbot python3-certbot-nginx sysstat iotop
 
 if [ -f $HOME/.inputrc ]; then
 	echo -e "\n$include ${SCRIPT_DIR}/ubuntu_inputrc" >> $HOME/.inputrc
